09a4359ab67c708b4fe4202ee97a973179dc6e42
Tail_Straw <maeva.gr@live.fr>
2024-06-20 22:41:03 +0200

    Config book routing

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 const app = express();
+const bookRoutes = require('./routes/book');
 
 mongoose.connect('mongodb+srv://root:toortoor@monvieuxgrimoire.ndwjlwp.mongodb.net/?retryWrites=true&w=majority&appName=MonVieuxGrimoire',
     { 
@@ -20,33 +22,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'Objet créé !'
-    });
-});
-
-app.use('/api/books', (req, res, next) => {
-    const books = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(books);
-});
+app.use('/api/book', bookRoutes);
 
 module.exports = app;
